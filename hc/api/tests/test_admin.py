afb72e5b5010c4f9b87ee3207260da0ffb9c26b5
WNjihia <dorisn172@gmail.com>
2017-05-15 17:33:21 +0300

    [Feature #145186283] Set Alice as super user and staff git commit -m [Feature

--- hc/api/tests/test_admin.py
+++ hc/api/tests/test_admin.py
@@ -1,5 +1,6 @@
 from hc.api.models import Channel, Check
 from hc.test import BaseTestCase
+from django.contrib.auth.models import User
 
 
 class ApiAdminTestCase(BaseTestCase):
@@ -8,13 +9,15 @@ class ApiAdminTestCase(BaseTestCase):
         super(ApiAdminTestCase, self).setUp()
         self.check = Check.objects.create(user=self.alice, tags="foo bar")
 
-
         ### Set Alice to be staff and superuser and save her :)
+        self.alice.is_superuser = True
+        self.alice.is_staff = True
+        self.alice.save()
 
     def test_it_shows_channel_list_with_pushbullet(self):
         self.client.login(username="alice@example.org", password="password")
 
         ch = Channel(user=self.alice, kind="pushbullet", value="test-token")
         ch.save()
-
+        
         ### Assert for the push bullet
