23038db78fb32cbf3e4a57d2107e0387e7e3c6e3
Kevin <kevin.tumbo39@gmail.com>
2017-05-18 13:05:15 +0300

    [Feature #145186283] Test it only allows post requests

--- hc/api/tests/test_pause.py
+++ hc/api/tests/test_pause.py
@@ -12,10 +12,10 @@ class PauseTestCase(BaseTestCase):
         r = self.client.post(url, "", content_type="application/json",
                              HTTP_X_API_KEY="abc")
 
-        ### Assert the expected status code and check's status
+        # Assert the expected status code and check's status
+
         self.assertEqual(r.status_code, 200)
         self.assertEqual(r.json()['status'], "paused")
-        print(vars(r))
 
     def test_it_validates_ownership(self):
         check = Check(user=self.bob, status="up")
@@ -27,4 +27,12 @@ class PauseTestCase(BaseTestCase):
 
         self.assertEqual(r.status_code, 400)
 
-        ### Test that it only allows post requests
+    def test_it_only_allows_post_requests(self):
+         # Test that it only allows post requests
+
+        check = Check(user=self.alice, status="up")
+        check.save()
+        url = "/api/v1/checks/%s/pause" % check.code
+        r = self.client.get(url, "", content_type="application/json",
+                            HTTP_X_API_KEY="abc")
+        self.assertEqual(r.status_code, 405)
