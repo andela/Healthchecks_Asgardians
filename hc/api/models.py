2ad980adb9767ed8efca635e39be60d7a37c8891
WNjihia <dorisn172@gmail.com>
2017-05-26 23:49:19 +0300

    add nag fields in model Check

--- hc/api/models.py
+++ hc/api/models.py
@@ -21,6 +21,7 @@ STATUSES = (
 )
 DEFAULT_TIMEOUT = td(days=1)
 DEFAULT_GRACE = td(hours=1)
+DEFAULT_NAG = td(hours=1)
 CHANNEL_KINDS = (("email", "Email"), ("webhook", "Webhook"),
                  ("hipchat", "HipChat"),
                  ("slack", "Slack"), ("pd", "PagerDuty"), ("po", "Pushover"),
@@ -52,6 +53,9 @@ class Check(models.Model):
     last_ping = models.DateTimeField(null=True, blank=True)
     alert_after = models.DateTimeField(null=True, blank=True, editable=False)
     status = models.CharField(max_length=6, choices=STATUSES, default="new")
+    nag = models.DurationField(default=DEFAULT_NAG, null=True)
+    last_nag = models.DateTimeField(null=True)
+    next_nag = models.DateTimeField(null=True, editable=False)
 
     def name_then_code(self):
         if self.name:
@@ -61,6 +65,11 @@ class Check(models.Model):
 
     def url(self):
         return settings.PING_ENDPOINT + str(self.code)
+
+    def set_next_nag(self):
+        now = timezone.now()
+        self.next_nag = now + self.nag
+        self.last_nag = now
 
     def log_url(self):
         return settings.SITE_ROOT + reverse("hc-log", args=[self.code])
