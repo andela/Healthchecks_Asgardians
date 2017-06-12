fad3a6aad6226e7da40d6bc4034835d7dd7693b4
Kevin <kevin.tumbo39@gmail.com>
2017-06-12 17:59:05 +0300

    [fx #145028403] fix views error

--- hc/blog/urls.py
+++ hc/blog/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     url(r'^post/create/$', views.create_post, name='create_post'),
     url(r'^post/(?P<pk>\d+)/view/$', views.post_view, name='post_view'),
     url(r'^blogpost/$', views.blog_list, name='blog_list'),
-    url(r'^blog/$', views.view_blog, name='blog'),
+    url(r'^blog/$', views.view_blog, name='hc-blog'),
 ]
