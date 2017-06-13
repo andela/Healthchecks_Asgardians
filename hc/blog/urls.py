155c9d35225781d46565e8286f1ed41e017080ca
dmkitui <daniel.kitui@andela.com>
2017-06-13 10:39:58 +0300

    [ft #145028403] Rename urls

--- hc/blog/urls.py
+++ hc/blog/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^post/create/$', views.create_post, name='create_post'),
-    url(r'^post/(?P<pk>\d+)/view/$', views.post_view, name='post_view'),
-    url(r'^blog/$', views.view_blog, name='blog'),
+    url(r'post/(?P<pk>\d+)/view/$', views.post_view, name='post_view'),
     url(r'^blog/$', views.blogs, name='hc-blogs'),
+    url(r'^blog/$', views.view_blog, name='blog')
 ]
